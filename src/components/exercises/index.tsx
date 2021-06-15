0b858dfd955260fef574fc9fcf7a8edb189eeed5
Carlos Zansavio <undefined751@gmail.com>
2021-06-15 19:20:51 -0300

    interface de exercise props

--- src/components/exercises/index.tsx
+++ src/components/exercises/index.tsx
@@ -2,15 +2,21 @@ import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Container } from "./styles";
 
+interface ExerciseProps {
+  id: string;
+}
+
 const Exercises = () => {
-  const params = useParams();
+  const params = useParams<ExerciseProps>();
+
   useEffect(() => {
-    console.log(params);
+    console.log(params.id);
   }, []);
 
   return (
     <Container>
       <h2>Exercises</h2>
+      {params.id}
     </Container>
   );
 };
